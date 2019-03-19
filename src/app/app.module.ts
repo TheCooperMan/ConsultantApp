dcb972fedc9c7d559c96de6cf7092488f2132a04
miassa <guezoutmiassa1@gmail.com>
2019-03-19 17:34:05 +0100

    user can edit name consultant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { ConsultantComponent } from './consultant/consultant.component';
 
@@ -10,7 +10,8 @@ import { ConsultantComponent } from './consultant/consultant.component';
     ConsultantComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
