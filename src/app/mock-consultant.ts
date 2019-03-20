57069f19efe1c51c8b10bc2735a34c8ec77debd0
miassa <guezoutmiassa1@gmail.com>
2019-03-20 10:47:35 +0100

    define a list of consultant

---
+++ src/app/mock-consultant.ts
@@ -0,0 +1,16 @@
+import { Consultant } from './consultant';
+
+export const CONSULTANT: Consultant[] = [
+  { lastname: 'Andalib', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Andaliba', firstname: 'Mr. Nicea', email: 'nani@gmail.com', phoneNumber: 7841269728 },
+  { lastname: 'Andalibx', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Andalibaw', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Andalibad', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Andalsad', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Andaldasb', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Andalsdab', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Anddalib', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Ansadib', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Andsdalib', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+  { lastname: 'Aadaadasa', firstname: 'Mr. Nice', email: 'nani@gmail.com', phoneNumber: 784126978 },
+];
