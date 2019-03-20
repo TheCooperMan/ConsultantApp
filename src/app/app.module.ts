e5e848baf0905c71ff77426f62697ef18c60ccbe
miassa <guezoutmiassa1@gmail.com>
2019-03-20 14:40:28 +0100

    create another component to stock details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 import { AppComponent } from './app.component';
 import { ConsultantComponent } from './consultant/consultant.component';
+import { ConsultantDetailComponent } from './consultant-detail/consultant-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConsultantComponent
+    ConsultantComponent,
+    ConsultantDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/consultant-detail/consultant-detail.component.ts
@@ -0,0 +1,16 @@
+import { Consultant } from './../consultant';
+import { Component, OnInit, Input } from '@angular/core';
+@Component({
+  selector: 'app-consultant-detail',
+  templateUrl: './consultant-detail.component.html',
+  styleUrls: ['./consultant-detail.component.css']
+})
+export class ConsultantDetailComponent implements OnInit {
+
+  @Input() consultant: Consultant;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
